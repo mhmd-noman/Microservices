b4a32b1d987f8b66d434f0df647810392ffefaa0
Muhammad Noman <muhammad.nauman54@outlook.com>
2022-12-25 02:52:35 +0500

    Adding Service Registry
    Service register would be managing the endpoints and will provide reverse proxy feature.

--- pmis-user-service/src/main/java/com/pmis/user/service/PmisUserServiceApplication.java
+++ pmis-user-service/src/main/java/com/pmis/user/service/PmisUserServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PmisUserServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PmisUserServiceApplication.class, args);
 	}
