3e73cb05e5d2b3420c737d2ef2baa07302476992
Muhammad Noman <muhammad.nauman54@outlook.com>
2022-12-31 03:06:55 +0500

    Adding API Gateway
    API Gateway service will serve all other services. Call will come through gateway not directly to the services.

---
+++ com-api-gateway/src/test/java/com/api/gateway/comapigateway/ComApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gateway.comapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
